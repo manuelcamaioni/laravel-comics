e32d5b550c6a1a2ae79e0d1cd239681221cee8c5
manuelcamaioni <camaioni.manuel@gmail.com>
2023-07-19 17:24:10 +0200

    created footer.blade.php and header.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>@yield('title', 'Homepage')</title>
 </head>
+
 <body>
     @vite('resources/js/app.js');
 </body>
+
 </html>
